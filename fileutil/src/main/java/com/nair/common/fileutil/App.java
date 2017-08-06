507a0db91aa5f339dc47043bde1a5ced05a191fc
Vaishakh Nair <vaishakhnair@Vaishakh.local>
2017-08-06 21:29:11 +0530

    initial commit for file util

---
+++ fileutil/src/main/java/com/nair/common/fileutil/App.java
@@ -0,0 +1,12 @@
+package com.nair.common.fileutil;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	
+    public static void main( String[] args ){
+        System.out.println( "Hello World!" );
+    }
+}
